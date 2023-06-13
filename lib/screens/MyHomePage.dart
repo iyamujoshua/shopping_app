1ffd98df7a15abc993c4d293172137bc56eb5008
joshamu20@gmail.com <joshamu20@gmail.com>
2023-06-13 23:33:15 +0100

    starting the ecommerce application

--- lib/screens/MyHomePage.dart
+++ lib/screens/MyHomePage.dart
@@ -8,11 +8,61 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  List catList = [
+    "All"
+        "Best Selling"
+        "Jackets"
+        "Shirt"
+        "Pant"
+        "Bag"
+  ];
+
+  List imgList = [
+    "Black Pant"
+    "Black T-Shirt"
+    "Cover"
+    "Ladies Bag"
+    "Warm Jacket"
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Hey"),
+      body: SingleChildScrollView(
+        child: SafeArea(
+          child: 
+        Padding(
+          padding: EdgeInsets.only(top: 20, left: 15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Padding(padding: EdgeInsets.only(right: 25),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    width: MediaQuery.of(context).size.width /1.5,
+                    decoration: BoxDecoration(
+                      color: Color(0xFFF7F8FA),
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+
+                    child: TextFormField(
+                      decoration: InputDecoration(
+                        label: Text("Find your product"),
+                        border: InputBorder.none,
+                        prefixIcon: Icon(Icons.search, size: 30, color: Colors.grey,)
+                      ),
+                    ),
+                  )
+                  
+                ],
+              ),
+              ),
+            ],
+          ),
+          )
+        ),
       ),
     );
   }
