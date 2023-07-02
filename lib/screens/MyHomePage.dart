a08ac1487fa4fee5e3febb22eea880ad03f276a9
joshamu20@gmail.com <joshamu20@gmail.com>
2023-07-02 18:41:21 +0100

    bottom navigation bar

--- lib/screens/MyHomePage.dart
+++ lib/screens/MyHomePage.dart
@@ -1,22 +1,18 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class MyHomePage extends StatelessWidget {
-   MyHomePage({super.key});
+import '../widgets/product_card.dart';
 
-  List catList = [
-        "All",
-        "Best Selling",
-        "Jackets",
-        "Shirt",
-        "Pant",
-        "Bag"
-  ];
+class MyHomePage extends StatelessWidget {
+  MyHomePage({super.key});
+
+  List catList = ["All", "Best Selling", "Jackets", "Shirt", "Pant", "Bag"];
 
   List imgList = [
-        "Black Pant",
-        "Black T-Shirt",
-        "Ladies Bag",
-        "Warm Jacket",
+    "Black Pant",
+    "Black T-Shirt",
+    "Ladies Bag",
+    "Warm Jacket",
   ];
 
   @override
@@ -67,50 +63,95 @@ class MyHomePage extends StatelessWidget {
                   ],
                 ),
               ),
-
               Container(
                 margin: EdgeInsets.only(right: 25, top: 20),
                 alignment: Alignment.center,
                 child: ClipRRect(
                   borderRadius: BorderRadius.circular(30),
-                  child: Image.asset("asset/cover.jpg",
-                  width: MediaQuery.of(context).size.width /1.2,
-                  fit: BoxFit.contain,
+                  child: Image.asset(
+                    "asset/cover.jpg",
+                    width: MediaQuery.of(context).size.width / 1.2,
+                    fit: BoxFit.contain,
                   ),
-                  
                 ),
               ),
-
               SingleChildScrollView(
                 scrollDirection: Axis.horizontal,
                 child: Padding(
-                  padding: EdgeInsets.only(top: 25,),
+                  padding: EdgeInsets.only(
+                    top: 25,
+                  ),
                   child: Row(
                     children: [
-                      for ( int i=0; i<catList.length; i++)
-                      Container(
-                        margin: EdgeInsets.all(8),
-                        padding: EdgeInsets.symmetric(
-                          vertical: 10, 
-                          horizontal: 18,
+                      for (int i = 0; i < catList.length; i++)
+                        Container(
+                          margin: EdgeInsets.all(8),
+                          padding: EdgeInsets.symmetric(
+                            vertical: 10,
+                            horizontal: 18,
                           ),
-                        decoration: BoxDecoration(
-                          color: catList[i]== "All"
-                          ?Color(0xFFFD725A)
-                          :Color(0xFFF7F8FA),
-                          
-                          borderRadius: BorderRadius.circular(18)
+                          decoration: BoxDecoration(
+                              color: catList[i] == "All"
+                                  ? Color(0xFFFD725A)
+                                  : Color(0xFFF7F8FA),
+                              borderRadius: BorderRadius.circular(18)),
+                          child: Text(
+                            catList[i],
+                            style: TextStyle(
+                              fontSize: 16,
+                              color: catList[i] == "All"
+                                  ? Color(0xFFF7F8FA)
+                                  : Colors.grey,
+                            ),
+                          ),
                         ),
-                        child: Text(catList[i]),
-                      ),
                     ],
                   ),
+                ),
+              ),
+              GridView.builder(
+                  physics: NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    childAspectRatio:
+                        (MediaQuery.of(context).size.width - 30 - 15) /
+                            (2 * 290),
+                    mainAxisSpacing: 45,
+                    crossAxisSpacing: 15,
                   ),
-              )
+                  itemCount: imgList.length,
+                  itemBuilder: (_, i) {
+                    if (i % 2 == 0) {
+                      return productCard(imgList[i]);
+                    }
+                    return OverflowBox(
+                      maxHeight: 299.0 + 70.0,
+                      child: Container(
+                        margin: EdgeInsets.only(top: 20),
+                        child: productCard(imgList[i]),
+                      ),
+                    );
+                  })
             ],
           ),
         )),
       ),
+      bottomNavigationBar:  BottomNavigationBar(
+        backgroundColor: Colors.white,
+        iconSize: 30,
+        unselectedItemColor: Colors.grey,
+        selectedItemColor: Color(0xFFFD725A),
+        currentIndex: 0,
+        onTap: (index){},
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: ""),
+          BottomNavigationBarItem(icon: Icon(CupertinoIcons.cart_fill), label: ""),
+          BottomNavigationBarItem(icon: Icon(Icons.favorite), label: ""),
+              BottomNavigationBarItem(icon: Icon(Icons.person), label: ""),
+        ],
+        ),
     );
   }
 }
---
+++ lib/widgets/product_card.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class productCard extends StatelessWidget {
+  // const productCard({super.key});
+  String img;
+  productCard(this.img);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
