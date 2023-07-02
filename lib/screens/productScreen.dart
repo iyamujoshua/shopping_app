3d136fb06a6004ba5b9e89ffc5167b2b815331fa
joshamu20@gmail.com <joshamu20@gmail.com>
2023-07-02 19:25:47 +0100

    productScreen

---
+++ lib/screens/productScreen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class productScreen extends StatelessWidget {
+  // const productScreen({super.key});
+  String img;
+  productScreen(this.img);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/widgets/product_card.dart
+++ lib/widgets/product_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../screens/productScreen.dart';
+
 class productCard extends StatelessWidget {
   // const productCard({super.key});
   String img;
@@ -16,50 +18,73 @@ class productCard extends StatelessWidget {
             child: Stack(
               children: [
                 InkWell(
-                  onTap: (){},
-                   child:   Image.asset("asset/${img}.png", 
-                   fit: BoxFit.cover,
-                    height: 230,)
-                ),
-                Padding(padding: EdgeInsets.all(8),
-                child: Align(
-                  alignment: Alignment.topRight,
-                  child: Container(
-                    padding: EdgeInsets.all(8),
-                    decoration: BoxDecoration(
-                      color: Color(0xFFF7F8FA),
-                      borderRadius: BorderRadius.circular(20),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => productScreen(img)));
+                    },
+                    child: Image.asset(
+                      "asset/${img}.png",
+                      fit: BoxFit.cover,
+                      height: 230,
+                    )),
+                Padding(
+                  padding: EdgeInsets.all(8),
+                  child: Align(
+                    alignment: Alignment.topRight,
+                    child: Container(
+                      padding: EdgeInsets.all(8),
+                      decoration: BoxDecoration(
+                        color: Color(0xFFF7F8FA),
+                        borderRadius: BorderRadius.circular(20),
+                      ),
+                      child: Icon(
+                        Icons.favorite,
+                        size: 20,
+                        color: Colors.red,
+                      ),
                     ),
-                    child: Icon(
-                      Icons.favorite,
-                      size: 20,
-                      color: Colors.red,
-                    ),
-                  ), 
                   ),
                 ),
-            
               ],
             ),
           ),
         ),
-        SizedBox(height: 10,),
-        Padding(padding: EdgeInsets.only(left: 5,),
-        child: Align(
-          alignment: Alignment.centerLeft,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(img, style: TextStyle(
-                fontSize: 19,
-                fontWeight: FontWeight.w500,
-                color: Colors.black.withOpacity(0.7),
-                ),
-                )
-            ],
-          ),
+        SizedBox(
+          height: 10,
         ),
-
+        Padding(
+          padding: EdgeInsets.only(
+            left: 5,
+          ),
+          child: Align(
+            alignment: Alignment.centerLeft,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  img,
+                  style: TextStyle(
+                    fontSize: 19,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.black.withOpacity(0.7),
+                  ),
+                ),
+                SizedBox(
+                  height: 5,
+                ),
+                Text(
+                  "\$300.54",
+                  style: TextStyle(
+                    fontSize: 19,
+                    fontWeight: FontWeight.w500,
+                    color: Colors.red.withOpacity(0.7),
+                  ),
+                ),
+              ],
+            ),
+          ),
         )
       ],
     );
