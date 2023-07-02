6a1c3e9d8304896624c652e64f14311af6a469af
joshamu20@gmail.com <joshamu20@gmail.com>
2023-07-02 20:22:57 +0100

    description

--- lib/screens/productScreen.dart
+++ lib/screens/productScreen.dart
@@ -9,18 +9,110 @@ class productScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: SafeArea(
-        child:Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              alignment: Alignment.topCenter,
-              height: MediaQuery.of(context).size.height,
-              decoration: BoxDecoration(
+          child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            alignment: Alignment.topCenter,
+            height: MediaQuery.of(context).size.height / 1.7,
+            decoration: BoxDecoration(
                 color: Color.fromARGB(255, 224, 224, 224),
+                image: DecorationImage(
+                    image: AssetImage("asset/${img}.png"), fit: BoxFit.cover)),
+            child: Padding(
+              padding: EdgeInsets.all(20),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Icon(
+                        Icons.arrow_back_ios_new,
+                        size: 22,
+                      ),
+                    ),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                    child: Container(
+                      padding: EdgeInsets.all(10),
+                      decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30),
+                      ),
+                      child: Icon(
+                        Icons.favorite,
+                        color: Colors.red,
+                        size: 22,
+                      ),
+                    ),
+                  ),
+                ],
               ),
-            )
-          ],
-        ) ),
+            ),
+          ),
+          SizedBox(
+            height: 10,
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 8, left: 15, right: 15),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(right: 5),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        img,
+                        style: TextStyle(
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      Text(
+                        "\$300.54",
+                        style: TextStyle(
+                          color: Colors.red.withOpacity(0.7),
+                          fontSize: 30,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 8,
+                ),
+                Text(
+                  "for women",
+                  style: TextStyle(
+                    color: Colors.black54,
+                    fontSize: 20,
+                  ),
+                ),
+                SizedBox(height: 8,),
+
+                   ///rating bar
+                   ///
+                SizedBox(height: 20,),
+                Text("Long Description of the product here", style: TextStyle(),)
+              ],
+            ),
+          )
+        ],
+      )),
     );
   }
 }
