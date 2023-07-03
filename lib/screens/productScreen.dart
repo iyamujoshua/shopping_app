c191b79f6bbbda9f34a1aca18cc79fc86c7c7d79
joshamu20@gmail.com <joshamu20@gmail.com>
2023-07-03 23:01:40 +0100

    update

--- lib/screens/productScreen.dart
+++ lib/screens/productScreen.dart
@@ -10,167 +10,169 @@ class productScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Container(
-            alignment: Alignment.topCenter,
-            height: MediaQuery.of(context).size.height / 1.7,
-            decoration: BoxDecoration(
-                color: Color.fromARGB(255, 224, 224, 224),
-                image: DecorationImage(
-                    image: AssetImage("asset/${img}.png"), fit: BoxFit.cover)),
-            child: Padding(
-              padding: EdgeInsets.all(20),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  InkWell(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Icon(
-                        Icons.arrow_back_ios_new,
-                        size: 22,
-                      ),
-                    ),
-                  ),
-                  InkWell(
-                    onTap: () {
-                      Navigator.pop(context);
-                    },
-                    child: Container(
-                      padding: EdgeInsets.all(10),
-                      decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(30),
-                      ),
-                      child: Icon(
-                        Icons.favorite,
-                        color: Colors.red,
-                        size: 22,
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          SizedBox(
-            height: 10,
-          ),
-          Padding(
-            padding: EdgeInsets.only(top: 8, left: 15, right: 15),
+      body: SingleChildScrollView(
+        child: SafeArea(
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Padding(
-                  padding: EdgeInsets.only(right: 5),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        img,
-                        style: TextStyle(
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Container(
+              alignment: Alignment.topCenter,
+              height: MediaQuery.of(context).size.height / 1.7,
+              decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 224, 224, 224),
+                  image: DecorationImage(
+                      image: AssetImage("asset/${img}.png"), fit: BoxFit.cover)),
+              child: Padding(
+                padding: EdgeInsets.all(20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Icon(
+                          Icons.arrow_back_ios_new,
+                          size: 22,
                         ),
                       ),
-                      Text(
-                        "\$300.54",
-                        style: TextStyle(
-                          color: Colors.red.withOpacity(0.7),
-                          fontSize: 30,
-                          fontWeight: FontWeight.bold,
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.pop(context);
+                      },
+                      child: Container(
+                        padding: EdgeInsets.all(10),
+                        decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(30),
+                        ),
+                        child: Icon(
+                          Icons.favorite,
+                          color: Colors.red,
+                          size: 22,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 8, left: 15, right: 15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Padding(
+                    padding: EdgeInsets.only(right: 5),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          img,
+                          style: TextStyle(
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          "\$300.54",
+                          style: TextStyle(
+                            color: Colors.red.withOpacity(0.7),
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    "for women",
+                    style: TextStyle(
+                      color: Colors.black54,
+                      fontSize: 20,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+      
+                  ///rating bar
+                  ///
+                  RatingBar.builder(unratedColor: Colors.grey, itemSize: 28, initialRating: 3.5,
+                  minRating: 1,
+                  direction: Axis.horizontal,
+                  itemPadding: EdgeInsets.symmetric(horizontal: 4),
+                  onRatingUpdate: (rating){},
+                  itemBuilder: (context, _)=>Icon(Icons.star, color: Colors.amber,)
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Text(
+                    "Long Description of the product here",
+                    style: TextStyle(
+                      fontSize: 16,
+                      color: Colors.black54,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+      
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          padding: EdgeInsets.all(18),
+                          decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 224, 219, 219),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Icon(CupertinoIcons.cart_fill),
+                        ),
+                      ),
+                      InkWell(
+                        onTap: () {},
+                        child: Container(
+                          padding:
+                              EdgeInsets.symmetric(vertical: 18, horizontal: 70),
+                          decoration: BoxDecoration(
+                            color: Colors.red.withOpacity(0.7),
+                            borderRadius: BorderRadius.circular(30),
+                          ),
+                          child: Text(
+                            "Buy Now",
+                            style: TextStyle(
+                              fontSize: 18,
+                              letterSpacing: 1,
+                              color: Colors.white,
+                            ),
+                          ),
                         ),
                       )
                     ],
-                  ),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                Text(
-                  "for women",
-                  style: TextStyle(
-                    color: Colors.black54,
-                    fontSize: 20,
-                  ),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-
-                ///rating bar
-                ///
-                RatingBar.builder(unratedColor: Colors.grey, itemSize: 28, initialRating: 3.5,
-                minRating: 1,
-                direction: Axis.horizontal,
-                itemPadding: EdgeInsets.symmetric(horizontal: 4),
-                onRatingUpdate: (rating){},
-                itemBuilder: (context, _)=>Icon(Icons.star, color: Colors.amber,)
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                Text(
-                  "Long Description of the product here",
-                  style: TextStyle(
-                    fontSize: 16,
-                    color: Colors.black54,
-                  ),
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: [
-                    InkWell(
-                      onTap: () {},
-                      child: Container(
-                        padding: EdgeInsets.all(18),
-                        decoration: BoxDecoration(
-                          color: Color.fromARGB(255, 224, 219, 219),
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        child: Icon(CupertinoIcons.cart_fill),
-                      ),
-                    ),
-                    InkWell(
-                      onTap: () {},
-                      child: Container(
-                        padding:
-                            EdgeInsets.symmetric(vertical: 18, horizontal: 70),
-                        decoration: BoxDecoration(
-                          color: Colors.red.withOpacity(0.7),
-                          borderRadius: BorderRadius.circular(30),
-                        ),
-                        child: Text(
-                          "Buy Now",
-                          style: TextStyle(
-                            fontSize: 18,
-                            letterSpacing: 1,
-                            color: Colors.white,
-                          ),
-                        ),
-                      ),
-                    )
-                  ],
-                )
-              ],
-            ),
-          )
-        ],
-      )),
+                  )
+                ],
+              ),
+            )
+          ],
+        )),
+      ),
     );
   }
 }
