e915e8c56910f168dd5c6fbd04e7f9c14027e3b3
joshamu20@gmail.com <joshamu20@gmail.com>
2023-07-04 15:55:20 +0100

    showcustomBottom

--- lib/screens/bottom_sheet.dart
+++ lib/screens/bottom_sheet.dart
@@ -1,10 +1,28 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class customBottomSheet extends StatelessWidget {
-  const customBottomSheet({super.key});
+  // const customBottomSheet({super.key});
+  List sizes = [
+    "S",
+    "M",
+    "L",
+    "XL",
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      padding: EdgeInsets.only(left: 20, right: 20, bottom: 50),
+      decoration: BoxDecoration(
+        color:Colors.white,
+        borderRadius: BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20),)
+      ),
+      child: Column(
+        children: [
+          
+        ],
+      ),
+    );
   }
 }
